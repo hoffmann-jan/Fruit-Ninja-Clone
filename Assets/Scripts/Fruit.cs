daed3763ec2d47cfb3266b9e6407c4f9a38e0b34
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2020-06-23 21:02:13 +0200

    add fruit explosion

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject SlicedFruitPrefab;
+    public float ExplosionRadius = 5;
+    public float ExplosionForceMin = 500;
+    public float ExplosionForceMax = 1000;
+
+
+    private void CreateSlicedFruit()
+    {
+        GameObject slicedFruit = Instantiate<GameObject>(SlicedFruitPrefab, transform.position, transform.rotation);
+        Rigidbody[] rigidBodies = slicedFruit.GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody rigidbodyPart in rigidBodies)
+        {
+            rigidbodyPart.transform.rotation = Random.rotation;
+            rigidbodyPart.AddExplosionForce(Random.Range(ExplosionForceMin, ExplosionForceMax), transform.position, ExplosionRadius);
+        }
+
+        Destroy(gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }
+    }
+}
