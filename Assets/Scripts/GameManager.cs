470155f594ec59890a6eb27e04ef4abe72cd729d
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2020-06-24 19:55:56 +0200

    add game over screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,19 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Score Elements")]
     public int Score;
     public Text ScoreText;
     public Text HighScoreText;
+
+    [Header("Game Over Elements")]
+    public GameObject GameOverPanel;
+
+
+    private void Awake()
+    {
+        GameOverPanel.SetActive(false);
+    }
 
     public void IncrementScore(int score)
     {
@@ -17,7 +27,27 @@ public class GameManager : MonoBehaviour
     {
         // Break Game
         Time.timeScale = 0;
-
+        GameOverPanel.SetActive(true);
         Debug.Log("Bomb HIT");
+    }
+
+    public void RestartGame()
+    {
+        Score = 0;
+        ScoreText.text = "0";
+
+        DeleteAllInteractables();
+
+        GameOverPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void DeleteAllInteractables()
+    {
+        foreach (var interactable in GameObject.FindGameObjectsWithTag("Interactable"))
+        {
+            Destroy(interactable);
+        }
+
     }
 }
