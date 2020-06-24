54688f94b8f53cd5a5b02fa9e1301bfdc8169e38
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2020-06-24 20:55:50 +0200

    set blade only active when mouse moving

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    public float MinimumVelocity = 0.1f;
+
     private Rigidbody2D rigidbodyBlade;
+    private Vector3 lastPosition;
+    private Collider2D bladeCollider2D;
 
     private void Awake()
     {
         rigidbodyBlade = GetComponent<Rigidbody2D>();
+        bladeCollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bladeCollider2D.enabled = IsMouseMoving();
         SetBladeToMouse();
     }
 
@@ -22,5 +28,18 @@ public class Blade : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = 10;
         rigidbodyBlade.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    bool IsMouseMoving()
+    {
+        Vector3 currentMousePosition = transform.position;
+        float travelled = (currentMousePosition - lastPosition).magnitude;
+        lastPosition = currentMousePosition;
+
+        if (travelled > MinimumVelocity)
+        {
+            return true;
+        }
+        return false;
     }
 }
